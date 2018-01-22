1a47e7d395ef8be8e9a31f9ad2da636e0eca4be9
Vyas, Dhaval <Dhaval.Vyas@t4gkick.com>
2018-01-22 12:51:57 -0400

    Removed Jest from package

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function yourFunction(param1, param2) {
+    return param1 + param2;
+}
+
+window.onload = function() {
+    let test = yourFunction(1, 2);
+    console.log(test);
+    alert(test);
+};
